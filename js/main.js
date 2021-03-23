6020e02207ae128c7f1dd5b7848970d85e1bc197
rafhaelgomes <rafhaell15@gmail.com>
2021-03-23 17:07:47 -0300

    master

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-$('.slide-principal').slick();
+$('.slide-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1,
+    autoplay: true,
+  autoplaySpeed: 2000
+});
